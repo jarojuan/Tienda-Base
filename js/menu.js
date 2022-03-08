a5b47039d65f3247621b1ab030ba26c8816fae4a
Juan <juan.308904@gmail.com>
2022-03-08 20:21:06 +0100

    Animacion del menu con js y css

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+//Variable para el botón de menú
+let btnMenu = document.getElementById('btnmenu');
+
+//Variable para el menú
+let menu = document.getElementById('menu');
+
+//Al hacer click en el bntMenu, la función aplica la clase mostrar a la variable menu
+btnMenu.addEventListener('click', function() {
+    'use strict';   //¿¿Se puede omitir??
+    menu.classList.toggle('mostrar');   //Si menu no tiene la clase mostrar se muestra al hacer click, si ya la tiene, la oculta. 
+})
